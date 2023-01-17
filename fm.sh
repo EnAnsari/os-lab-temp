ed5e3c848d1af5875a4a58dc72641aa035f20832
EnAnsari <rahmat2022a@gmail.com>
2023-01-17 17:57:55 +0330

    update

---
+++ fm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# github.com/EnAnsari
+# Rahmat2022a@gmail.com
+
+# Print help
+if [[ $1 = "--help" ]]; then
+    echo '[F]ile [M]anager for Linux'
+    echo 'Writer: Rahmatollah Ansari'
+    echo 'Student Code: 9912377331'
+    echo 'github: github.com/EnAnsari'
+    echo 'Email: Rahmat2022a@gmail.com'
+    echo ''
+fi
